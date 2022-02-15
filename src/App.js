a50c9b3a5e9b904ce37f55a8e041295a82f66b34
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2022-02-15 18:30:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+const convert = require("ethereum-unit-converter");
 
 function App() {
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState(null);
+
+  const getBalance = async () => {
+    const result = await axios(
+      `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+    );
+
+    const weiBalance = result.data.result;
+    const etherBalance = convert(weiBalance, "wei", "ether");
+
+    console.log(etherBalance)
+
+    setBalance(`ETH ${etherBalance}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <main className="app__main">
+        <h1>Get Ξther Balance</h1>
+
+        <input
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          type="text"
+          placeholder="Enter the address"
+          required={true}
+        />
+
+        <button
+          onClick={getBalance}
+          disabled={!address || address.substring(0, 2) !== "0x"}
         >
-          Learn React
-        </a>
-      </header>
+          Find balance
+        </button>
+
+        <div className="app__balance">
+          <p>The balance is {balance}</p>
+        </div>
+      </main>
     </div>
   );
 }
